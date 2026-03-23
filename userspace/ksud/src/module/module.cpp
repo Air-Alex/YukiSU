5529bfdbc1768921628f4aa1ac238b752a11b474
Anan <an@anatdx.com>
2026-03-23 13:49:24 +0800

    ksud: add missing installer wrapper helpers

--- userspace/ksud/src/module/module.cpp
+++ userspace/ksud/src/module/module.cpp
@@ -362,29 +362,15 @@ ui_print() {
   echo "$1"
 }
 
-abort() {
-  ui_print "$1"
-  exit 1
+toupper() {
+  echo "$@" | tr '[:lower:]' '[:upper:]'
 }
 
-set_perm() {
-  chown $2:$3 $1 2>/dev/null
-  chmod $4 $1 2>/dev/null
-}
-
-set_perm_recursive() {
-  find $1 -type d 2>/dev/null | while read dir; do
-    set_perm $dir $2 $3 $4
-  done
-  find $1 -type f -o -type l 2>/dev/null | while read file; do
-    set_perm $file $2 $3 $5
-  done
-}
-
-mktouch() {
-  mkdir -p ${1%/*} 2>/dev/null
-  [ -z $2 ] && touch $1 || echo $2 > $1
-  chmod 644 $1
+grep_cmdline() {
+  local REGEX="s/^$1=//p"
+  { echo $(cat /proc/cmdline)$(sed -e 's/[^\"]//g' -e 's/\"\"//g' /proc/cmdline) | xargs -n 1; \
+    sed -e 's/ = /=/g' -e 's/, /,/g' -e 's/\"//g' /proc/bootconfig; \
+  } 2>/dev/null | sed -n "$REGEX"
 }
 
 grep_prop() {
@@ -392,7 +378,7 @@ grep_prop() {
   shift
   local FILES=$@
   [ -z "$FILES" ] && FILES='/system/build.prop'
-  cat $FILES 2>/dev/null | sed -n "$REGEX" | head -n 1
+  cat $FILES 2>/dev/null | dos2unix | sed -n "$REGEX" | head -n 1 | xargs
 }
 
 grep_get_prop() {
@@ -404,29 +390,273 @@ grep_get_prop() {
   fi
 }
 
-# Detect API level and architecture
-API=$(getprop ro.build.version.sdk)
-ABI=$(getprop ro.product.cpu.abi)
-if [ "$ABI" = "x86" ]; then
-  ARCH=x86
-  ABI32=x86
-  IS64BIT=false
-elif [ "$ABI" = "arm64-v8a" ]; then
-  ARCH=arm64
-  ABI32=armeabi-v7a
-  IS64BIT=true
-elif [ "$ABI" = "x86_64" ]; then
-  ARCH=x64
-  ABI32=x86
-  IS64BIT=true
-else
-  ARCH=arm
-  ABI=armeabi-v7a
-  ABI32=armeabi-v7a
-  IS64BIT=false
-fi
+is_mounted() {
+  grep -q " $(readlink -f $1) " /proc/mounts 2>/dev/null
+  return $?
+}
 
-export API ARCH ABI ABI32 IS64BIT
+recovery_cleanup() {
+  :
+}
+
+ensure_bb() {
+  :
+}
+
+recovery_actions() {
+  :
+}
+
+setup_flashable() {
+  ensure_bb
+  $BOOTMODE && return
+  recovery_actions
+}
+
+abort() {
+  ui_print "$1"
+  $BOOTMODE || recovery_cleanup
+  [ ! -z "$MODPATH" ] && rm -rf "$MODPATH"
+  rm -rf "$TMPDIR"
+  exit 1
+}
+
+print_title() {
+  local len line1len line2len bar
+  line1len=$(echo -n $1 | wc -c)
+  line2len=$(echo -n $2 | wc -c)
+  len=$line2len
+  [ $line1len -gt $line2len ] && len=$line1len
+  len=$((len + 2))
+  bar=$(printf "%${len}s" | tr ' ' '*')
+  ui_print "$bar"
+  ui_print " $1 "
+  [ "$2" ] && ui_print " $2 "
+  ui_print "$bar"
+}
+
+check_sepolicy() {
+  /data/adb/ksud sepolicy check "$1"
+  return $?
+}
+
+set_perm() {
+  chown $2:$3 $1 || return 1
+  chmod $4 $1 || return 1
+  local CON=$5
+  [ -z "$CON" ] && CON=u:object_r:system_file:s0
+  chcon $CON $1 || return 1
+}
+
+set_perm_recursive() {
+  find $1 -type d 2>/dev/null | while read dir; do
+    set_perm $dir $2 $3 $4 $6
+  done
+  find $1 -type f -o -type l 2>/dev/null | while read file; do
+    set_perm $file $2 $3 $5 $6
+  done
+}
+
+mktouch() {
+  mkdir -p ${1%/*} 2>/dev/null
+  [ -z $2 ] && touch $1 || echo $2 > $1
+  chmod 644 $1
+}
+
+mark_remove() {
+  mkdir -p ${1%/*} 2>/dev/null
+  mknod $1 c 0 0
+  chmod 644 $1
+}
+
+request_size_check() {
+  reqSizeM=`du -ms "$1" | cut -f1`
+}
+
+request_zip_size_check() {
+  reqSizeM=`unzip -l "$1" | tail -n 1 | awk '{ print int(($1 - 1) / 1048576 + 1) }'`
+}
+
+boot_actions() { return; }
+
+# find_block [partname...]
+find_block() {
+  local BLOCK DEV DEVICE DEVNAME PARTNAME UEVENT
+  for BLOCK in "$@"; do
+    DEVICE=`find /dev/block \( -type b -o -type c -o -type l \) -iname $BLOCK | head -n 1` 2>/dev/null
+    if [ ! -z $DEVICE ]; then
+      readlink -f $DEVICE
+      return 0
+    fi
+  done
+  for UEVENT in /sys/dev/block/*/uevent; do
+    DEVNAME=`grep_prop DEVNAME $UEVENT`
+    PARTNAME=`grep_prop PARTNAME $UEVENT`
+    for BLOCK in "$@"; do
+      if [ "$(toupper $BLOCK)" = "$(toupper $PARTNAME)" ]; then
+        echo /dev/block/$DEVNAME
+        return 0
+      fi
+    done
+  done
+  for DEV in "$@"; do
+    DEVICE=`find /dev \( -type b -o -type c -o -type l \) -maxdepth 1 -iname $DEV | head -n 1` 2>/dev/null
+    if [ ! -z $DEVICE ]; then
+      readlink -f $DEVICE
+      return 0
+    fi
+  done
+  return 1
+}
+
+setup_mntpoint() {
+  local POINT=$1
+  [ -L $POINT ] && mv -f $POINT ${POINT}_link
+  if [ ! -d $POINT ]; then
+    rm -f $POINT
+    mkdir -p $POINT
+  fi
+}
+
+mount_name() {
+  local PART=$1
+  local POINT=$2
+  local FLAG=$3
+  setup_mntpoint $POINT
+  is_mounted $POINT && return
+  mount $FLAG $POINT 2>/dev/null
+  if ! is_mounted $POINT; then
+    local BLOCK=$(find_block $PART)
+    mount $FLAG $BLOCK $POINT || return
+  fi
+  ui_print "- Mounting $POINT"
+}
+
+mount_ro_ensure() {
+  $BOOTMODE && return
+  local PART=$1
+  local POINT=$2
+  mount_name "$PART" $POINT '-o ro'
+  is_mounted $POINT || abort "! Cannot mount $POINT"
+}
+
+mount_partitions() {
+  SLOT=`grep_cmdline androidboot.slot_suffix`
+  if [ -z $SLOT ]; then
+    SLOT=`grep_cmdline androidboot.slot`
+    [ -z $SLOT ] || SLOT=_${SLOT}
+  fi
+  [ -z $SLOT ] || ui_print "- Current boot slot: $SLOT"
+
+  if is_mounted /system_root; then
+    umount /system 2>/dev/null
+    umount /system_root 2>/dev/null
+  fi
+  mount_ro_ensure "system$SLOT app$SLOT" /system
+  if [ -f /system/init -o -L /system/init ]; then
+    SYSTEM_ROOT=true
+    setup_mntpoint /system_root
+    if ! mount --move /system /system_root; then
+      umount /system
+      umount -l /system 2>/dev/null
+      mount_ro_ensure "system$SLOT app$SLOT" /system_root
+    fi
+    mount -o bind /system_root/system /system
+  else
+    SYSTEM_ROOT=false
+    grep ' / ' /proc/mounts | grep -qv 'rootfs' || grep -q ' /system_root ' /proc/mounts && SYSTEM_ROOT=true
+  fi
+  [ -L /system/vendor ] && mount_name vendor$SLOT /vendor '-o ro'
+  $SYSTEM_ROOT && ui_print "- Device is system-as-root"
+
+  if ! $BOOTMODE; then
+    mount_name "cache cac" /cache
+    mount_name metadata /metadata
+    mount_name persist /persist
+  fi
+}
+
+check_managed_features() {
+  local PROP_FILE=$1
+  local MANAGED_FEATURES=$(grep_prop managedFeatures "$PROP_FILE")
+
+  [ -z "$MANAGED_FEATURES" ] && return 0
+
+  ui_print "- Checking managed features: $MANAGED_FEATURES"
+
+  echo "$MANAGED_FEATURES" | tr ',' '\n' | while read -r feature; do
+    feature=$(echo "$feature" | xargs)
+    [ -z "$feature" ] && continue
+
+    local status=$(/data/adb/ksud feature check "$feature" 2>/dev/null)
+
+    case "$status" in
+      "unsupported")
+        ui_print "! WARNING: Feature '$feature' is NOT SUPPORTED by kernel"
+        ui_print "!          This module may not work correctly!"
+        ;;
+      "managed")
+        ui_print "! WARNING: Feature '$feature' is already MANAGED by another module"
+        ui_print "!          Feature conflicts may occur!"
+        ;;
+      "supported")
+        ui_print "- Feature '$feature' is supported and available"
+        ;;
+      *)
+        ui_print "! WARNING: Unable to check feature '$feature' status"
+        ;;
+    esac
+  done
+}
+
+handle_partition() {
+  if [ ! -e $MODPATH/system/$1 ]; then
+    return
+  fi
+
+  if [ -d "/$1" ] && [ ! -L "/$1" ]; then
+    ui_print "- Handle partition /$1"
+    mv -f $MODPATH/system/$1 $MODPATH/$1 && ln -sf ../$1 $MODPATH/system/$1
+  fi
+}
+
+is_legacy_script() {
+  unzip -l "$ZIPFILE" install.sh | grep -q install.sh
+  return $?
+}
+
+NVBASE=${NVBASE:-/data/adb}
+TMPDIR=${TMPDIR:-/dev/tmp}
+POSTFSDATAD=${POSTFSDATAD:-$NVBASE/post-fs-data.d}
+SERVICED=${SERVICED:-$NVBASE/service.d}
+export NVBASE TMPDIR POSTFSDATAD SERVICED
+
+api_level_arch_detect() {
+  API=$(grep_get_prop ro.build.version.sdk)
+  ABI=$(grep_get_prop ro.product.cpu.abi)
+  if [ "$ABI" = "x86" ]; then
+    ARCH=x86
+    ABI32=x86
+    IS64BIT=false
+  elif [ "$ABI" = "arm64-v8a" ]; then
+    ARCH=arm64
+    ABI32=armeabi-v7a
+    IS64BIT=true
+  elif [ "$ABI" = "x86_64" ]; then
+    ARCH=x64
+    ABI32=x86
+    IS64BIT=true
+  else
+    ARCH=arm
+    ABI=armeabi-v7a
+    ABI32=armeabi-v7a
+    IS64BIT=false
+  fi
+
+  export API ARCH ABI ABI32 IS64BIT
+}
+
+api_level_arch_detect
 
 # Now source the actual customize.sh
 . )WRAPPER";
@@ -447,6 +677,9 @@ export API ARCH ABI ABI32 IS64BIT
         setenv("MODPATH", modpath.c_str(), 1);
         setenv("ZIPFILE", zipfile.c_str(), 1);
         setenv("NVBASE", "/data/adb", 1);
+        setenv("TMPDIR", "/dev/tmp", 1);
+        setenv("POSTFSDATAD", "/data/adb/post-fs-data.d", 1);
+        setenv("SERVICED", "/data/adb/service.d", 1);
         setenv("BOOTMODE", "true", 1);
 
         execl(busybox.c_str(), "sh", wrapper.c_str(), nullptr);
