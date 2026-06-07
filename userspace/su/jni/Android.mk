31d8cb340324063a416662a512018bc04c4e8a80
Anan <an@anatdx.com>
2026-06-08 07:31:42 +0800

    refactor: remove obsolete userspace su build files and configurations

--- userspace/su/jni/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := su
-LOCAL_SRC_FILES := su.c
-# Size optimization (Os + LTO)
-LOCAL_CFLAGS := -Os -DNDEBUG -ffunction-sections -fdata-sections -flto
-LOCAL_LDFLAGS := -Wl,--gc-sections -Wl,--strip-all -flto
-include $(BUILD_EXECUTABLE)
